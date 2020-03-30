701a4900da5ac3dafb09eff61ba3893a56e97b1d
PavithraKrishna-03 <pavithrakrishnamoorthy98@gmail.com>
2020-03-30 22:55:35 +0530

    simple.java

---
+++ simple.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class simple
+ {
+   public static void main(String args[]) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter 1st number");
+	int n=sc.nextInt();
+	System.out.println("enter 2nd number");
+	int q=sc.nextInt();
+ 	int w=n+q;
+	System.out.println("the output :"+w);
+	}
+}
+
